1925ba8c79662d03f69ade0a47877ca63f747012
jbyepez <jbyepezh@unal.edu.co>
2019-03-18 21:08:24 -0500

    modelo funcionando

--- digitos_script_red_neuronal.R
+++ digitos_script_red_neuronal.R
@@ -5,16 +5,7 @@ trainingData <- read.csv("./data/mnist_train.csv",header = FALSE)
 testingData <- read.csv("./data/mnist_test.csv",header = FALSE)
 
 
-# #Ver uno de los digitos como imagen
-# library(magick)
-# im <- function(x,m){ #Funcion para ver el digito
-#   plot(image_read(aperm(array(as.numeric(m[x,2:785]),c(28,28,1)),c(2,1,3)))) #Muestra la imagen en el viewer
-#   return(m[x,1]) #Muestra el numero respectivo en consola
-# }
-# #Imagen 4 de trainingData
-# im(4,trainingData)
-# #Imagen 8 de testingData
-# im(8,testingData)
+
 library(keras)
 
 #Labels
@@ -23,32 +14,54 @@ trainLabels <-to_categorical(trainY)
 testY <- as.matrix(testingData["V1"])
 testLabels <- to_categorical(testY)
 
-trainingData <- subset(trainingData, select = -c(V1))
-trainingData <- as.matrix(trainingData)
-trainingData <- array(trainingData, c(60000,784,1))
-testingData <- subset(testingData, select = -c(V1))
+trainingData <- array(as.matrix(subset(trainingData, select = -c(V1))), c(60000,784))
+testingData <- array(as.matrix(subset(testingData, select = -c(V1))), c(10000,784))
 
-model <- keras_model_sequential() %>% 
-  layer_conv_1d(filters = 8, kernel_size = 3, activation = 'relu', input_shape = c(784, 1)) %>% 
-  layer_conv_1d(filters = 8, kernel_size = 3, activation = 'relu') %>% 
-  layer_max_pooling_1d(pool_size = 3) %>% 
-  layer_conv_1d(filters = 16, kernel_size = 3, activation = 'relu') %>% 
-  layer_conv_1d(filters = 16, kernel_size = 3, activation = 'relu') %>% 
-  layer_global_average_pooling_1d() %>% 
-  layer_dropout(rate = 0.5) %>% 
-  layer_dense(units = 10, activation = 'sigmoid') %>% 
-  compile(
-    loss = 'binary_crossentropy',
-    optimizer = 'rmsprop',
-    metrics = c('accuracy')
-  )
+# #Ver uno de los digitos como imagen
+# library(magick)
+# im <- function(x,m,n){ #Funcion para ver el digito
+#   plot(image_read(aperm(array(as.numeric(m[x,,1]),c(28,28,1)),c(2,1,3)))) #Muestra la imagen en el viewer
+#   return(n[x,1]) #Muestra el numero respectivo en consola
+# }
+# #Imagen 4 de trainingData
+# im(4,trainingData,trainY)
+# #Imagen 8 de testingData
+# im(8,testingData,testY)
+
+
+# Crear modelo
+model <- keras_model_sequential() %>%
+  layer_dense(128, input_shape=c (784,NULL), activation = 'relu') %>%
+  layer_dropout(0.2) %>%
+  layer_dense(10, activation = 'relu') %>%
+  layer_dropout(0.2) %>%
+  layer_dense(10, activation = 'softmax') %>%
+  compile(loss='categorical_crossentropy', metrics='accuracy', optimizer='adam')
+
 
 # Fit model
-history <- model %>%
+history2 <- model %>%
   fit(trainingData,
       trainLabels,
-      epochs = 50,
-      batch_size = 32,
+      epochs = 100,
+      batch_size = 2048,
       validation_split = 0.2)
       #validation_data = list(test, testLabels))
 plot(history)
+
+save_model_hdf5(model, "model")
+
+model <- load_model_hdf5("model")
+
+# Evaluation & Prediction - train data
+model %>% evaluate(trainingData, trainLabels)
+pred <- model %>% predict_classes(trainingData)
+a <- table(Predicted = pred, Actual = trainY)
+
+prob <- model %>% predict_proba(trainingData)
+cbind(prob, Predicted_class = pred, Actual = trainY)
+
+# Evaluation & Prediction - test data
+model %>% evaluate(testingData, testLabels)
+pred <- model %>% predict_classes(testingData)
+b <- table(Predicted = pred, Actual = testY)

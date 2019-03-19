d94e0def359db4fd468294beb1c8ec3b97997377
jbyepez <jbyepezh@unal.edu.co>
2019-03-18 23:34:16 -0500

    Update digitos_script_red_neuronal.R

--- digitos_script_red_neuronal.R
+++ digitos_script_red_neuronal.R
@@ -39,7 +39,7 @@ model <- keras_model_sequential() %>%
   compile(loss='categorical_crossentropy', metrics='accuracy', optimizer='adam')
 
 
-# Fit model
+# Entrenar modelo
 history2 <- model %>%
   fit(trainingData,
       trainLabels,
@@ -49,19 +49,44 @@ history2 <- model %>%
       #validation_data = list(test, testLabels))
 plot(history)
 
+#Guardar
 save_model_hdf5(model, "model")
 
+#Cargar
 model <- load_model_hdf5("model")
 
 # Evaluation & Prediction - train data
 model %>% evaluate(trainingData, trainLabels)
-pred <- model %>% predict_classes(trainingData)
-a <- table(Predicted = pred, Actual = trainY)
+predtrain <- model %>% predict_classes(trainingData)
+a <- table(Predicted = predtrain, Actual = trainY)
 
 prob <- model %>% predict_proba(trainingData)
-cbind(prob, Predicted_class = pred, Actual = trainY)
+cbind(prob, Predicted_class = predtrain, Actual = trainY)
+
+# Conjunto de imagenes de entrenamiento mal predichas
+badPredTrainData <- cbind(predicted = predtrain, correct = trainY, pos = c(1:60000))
+badPredTrainData <- subset(badPredTrainData, badPredTrainData[,1] != badPredTrainData[,2])
 
 # Evaluation & Prediction - test data
 model %>% evaluate(testingData, testLabels)
-pred <- model %>% predict_classes(testingData)
-b <- table(Predicted = pred, Actual = testY)
+predtest <- model %>% predict_classes(testingData)
+b <- table(Predicted = predtest, Actual = testY)
+
+prob <- model %>% predict_proba(testingData)
+cbind(prob, Predicted_class = predtest, Actual = testY)
+
+# Conjunto de imagenes de prueba mal predichas
+badPredTestData <- cbind(predicted = predtest, correct = testY, pos = c(1:10000))
+badPredTestData <- subset(badPredTestData, badPredTestData[,1] != badPredTestData[,2])
+
+#Funcion para ver uno de los digitos mal predichos
+library(magick)
+badpred <- function(predictions,dataset,pos){ #Funcion para ver el digito
+  plot(image_read(aperm(array(as.numeric(dataset[predictions[pos,3],]),c(28,28,1)),c(2,1,3)))) #Muestra la imagen en el viewer
+  print(paste("predicho:",predictions[pos,1]))
+  paste("real:    ",predictions[pos,2])
+  #return(n[x,1]) #Muestra el numero respectivo en consola
+}
+
+#Ejemplo de uso: Imagen mal predicha 4 de los datos de prueba
+badpred(badPredTestData,testingData,4)
